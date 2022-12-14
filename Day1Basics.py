e1518edcc1a68358dde17dbe2d8c896452e8d722
Shashank Phiske <111897076+Shashankphiske@users.noreply.github.com>
2022-12-14 10:41:12 -0800

    Add files via upload

---
+++ Day1Basics.py
@@ -0,0 +1,74 @@
+'''
+PYTHON:
+Python is a popular programming language. It was created by Guido van Rossum, and released in 1991.
+
+It is used for:
+web development (server-side),
+software development,
+mathematics,
+system scripting.
+
+Python can be used on a server to create web applications.
+Python can be used alongside software to create workflows.
+Python can connect to database systems. It can also read and modify files.
+Python can be used to handle big data and perform complex mathematics.
+Python can be used for rapid prototyping, or for production-ready software development.
+
+Python works on different platforms (Windows, Mac, Linux, Raspberry Pi, etc).
+Python has a simple syntax similar to the English language.
+Python has syntax that allows developers to write programs with fewer lines than some other programming languages.
+Python runs on an interpreter system, meaning that code can be executed as soon as it is written. This means that prototyping can be very quick.
+Python can be treated in a procedural way, an object-oriented way or a functional way.'''
+
+#Eg:
+print("Hello,World!")
+
+'''Python Indentation : Indentation refers to the spaces at the beginning of a code line
+                        Python uses indentation to indicate a block of code'''
+'''Python Variables : Variables are data storages and created when we assign any value to it
+                      variables dont need to be assigned any type to it and they can even change type   
+                      Variables are of different types String,Float,Integers and Boolean etc
+                      Eg ; x = 5  
+   Comments : Python has commenting capability for the purpose of in code documentation
+              comments starts with a # or multiple line comments can be added in ''' '''   
+              comments can be used to explain python code
+              it can also prevent python from executing code      
+   Type casting : If you want to change the data type of a variable ,this can be done by type casting
+                  Eg : x = int("4")
+   Type function : You can get the data type of any variable with type() function
+                  Eg : x = 5
+                       print(type(x))
+   Variable names : A variable name shouldnt start with a number 
+                    It shouldnt have spaces in its name
+                    It should be more descriptive, easy to understand and efficient
+   Mutli assignments : We can assign multiple values to multiple variables in a single line
+                       Eg : x,y,z = 2,3,4
+   One value to multiple assignments : Eg : x = y = z = 3
+   
+   Print function : print() function is often used to output variables or to display something
+   
+   Int data type : Int,integer or whole number ,positive or negative ,without decimalsof unlimited length
+   Float data type : Float or floating point or decimal number is a data type which has decimal point to it 
+   Complex data type : complex data types are assigned with a 'j' as a imaginary part
+   String data type : Strings in python are surrounded by double quotes or single quotes
+                      We can assign multi line strings to a variable by using three quotation marks
+                      We can attach to string to each other as "shashank" + "phiske"
+                      this is called as string concatenation
+   Boolean values : In programming we need to kno if an expression or condition is True or False
+                    thus the evaluation of any expression is True or False
+                    When we compare two values the expression is evaluated
+                    Eg : print(3>4) answer will False
+                    Almost any value is evaluated to True if it has some sort of content.   
+                    Any string is True, except empty strings.
+                    Any number is True, except 0.
+                    Any list, tuple, set, and dictionary are True, except empty ones.
+   Python operators : Operators are used to perform a operation on a variable and values
+                      python has :
+                        Arithmetic operators
+                        Assignment operators
+                        Comparison operators
+                        Logical operators
+                        Identity operators
+                        Membership operators
+                        Bitwise operators
+                                     '''                        
