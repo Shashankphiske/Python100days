aa06242b41413e83da0d10cfb865bf506dc51653
Shashank Phiske <111897076+Shashankphiske@users.noreply.github.com>
2022-12-15 05:30:23 -0800

    Add files via upload

---
+++ Day2Operators.py
@@ -0,0 +1,56 @@
+'''
+Arithmetic operators : Arithmetic operators are used with numeric values to perform common mathematical operations
+                       + : addition
+                       - : substraction
+                       * : multiplication
+                       / : division
+                       % : modulus
+                       ** : exponential
+                       // : Floor division
+Assignment operators : Assignment operators are used to assign values to variables
+                       = : x=5
+                       += : x+=5
+                       -= : x-=5
+                       *= : x*=5
+                       /= : x/=5
+                       //=
+                       **=
+                       &=
+                       !=
+                       ^=
+                       >>=
+                       <<=
+Comparison operator : Comparison operators are used to compare two values
+                      ==
+                      !=    
+                      >
+                      <
+                      >=
+                      <=
+Python logical operators : logical operators are used to combine conditional statements
+                           and (returns True if both statements are True)
+                           or (returns True if one of the statements in True)
+                           not (reverses the result)
+Python Identity operator : identity operators are used to compare the objects,not if they are equal,but if they
+                           are actualy the same objects,with the same memory location
+                           is (returns True if both objects are True
+                               eg : x = [2,3,4]
+                                    y = [2,3,4]
+                                    z = x
+                                    print(z is x) True
+                                    print(y is x) False)
+                           is not
+Python membership operators : Membership operators are used to test if a sequence is present in an object
+                              in (returns True if sequence available in object
+                                  for eg: x = [2,3,4] 
+                                          y = 2
+                                          print(y in x)
+                              not in
+Python bitwise operators : Bitwise operators are used to compare binary numbers
+                           &  (And : sets each bit to 1 if both bits are 1)
+                           |  (OR : sets each bit to 1 if one of the two bit is 1)
+                           ^  (XOR : sets each bit to 1 if only one of two bits is 1 )
+                           ~  (NOT : inverts all the bits)
+                           << (Zero fill left : shift left by pushing zeros in from right and let the leftmost bit fall off)
+                           >> (Signed right shift : shift right by pushing copies of leftmost bit in from the left
+                               and let the rightmost bit fall off )                                                              '''
