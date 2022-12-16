b682fc56f62944f640067d7617bd3e2e079b89e0
Shashank Phiske <111897076+Shashankphiske@users.noreply.github.com>
2022-12-16 11:09:05 -0800

    Add files via upload

---
+++ Day4Tuples.py
@@ -0,0 +1,7 @@
+'''
+Tuples : Tuple items are ordered,unchangeable and allows duplicate items
+         tuples are written with ()   
+         Tuple items are indexed ,the first item has index[0]
+         Tuples are unchangeable
+         to make a tuple with one item we need to add a comma after the item for python to understand
+         to change a tuple first typecast it into a list and then change it then convert it back to tuple   '''
