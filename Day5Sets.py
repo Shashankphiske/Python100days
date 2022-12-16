86d2ecdfa9a19af7bdd38ba32f55000fe1a6ab8c
Shashank Phiske <111897076+Shashankphiske@users.noreply.github.com>
2022-12-16 11:09:22 -0800

    Add files via upload

---
+++ Day5Sets.py
@@ -0,0 +1,13 @@
+'''
+Sets : sets are used to store multiple items in a variable
+       a set is a collection which is unordered ,unchangeable and unindexed
+       but you can remove or add items
+       sets are written with {}
+       sets doesnt have any duplicates
+       we can add sets with .add() method
+       in solving problems for maths , physics etc we usually take the union or intersection of sets
+       The .intersection_update() method will keep only those items which are present in both sets
+       The .intersection() method will return all those items which are available in both sets
+       The .symmetric_difference_update() will keep those elements which are not available in both sets
+       The .symmetric_difference() will return a set with elements which are not available in both sets
+         '''
