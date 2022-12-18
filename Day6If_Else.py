28412de9c95c7e85bbad17c2d4fa485a7dd7e804
Shashank Phiske <111897076+Shashankphiske@users.noreply.github.com>
2022-12-18 03:19:35 -0800

    Add files via upload

---
+++ Day6If_Else.py
@@ -0,0 +1,17 @@
+'''
+python supports the usual logical conditions from mathematics
+these conditions can be used in if-else statements
+
+if statement : if condition: (executed only if condition is True)
+                    block of code 
+               an if statement can exist alone
+elif statement : the if statement is False we can try this statement other than the if condition
+                 elif condition:
+                     block of code
+                 elif statement cannot exist alone it needs if statement with it
+else statement : it means do this instead if the conditions in if and elif arent met
+                 else:
+                    block of code
+                 else statement cant exist alone it needs if statement
+nested if : we can make multiple if statement inside another if statement
+                                                              '''
