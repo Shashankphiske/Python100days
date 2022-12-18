301c737a905164043f0a789bbe6f1120de4cbb43
Shashank Phiske <111897076+Shashankphiske@users.noreply.github.com>
2022-12-18 03:19:52 -0800

    Add files via upload

---
+++ Day7Whileloops.py
@@ -0,0 +1,9 @@
+'''
+Python loops : 
+python has two loops
+    while loops
+    for loops
+
+While loop : with the while loop we can execute a set of statements as long as a condition is True
+             we can use a else statement with while loop
+             if the while loop condition is False then the else statement is executed   '''
