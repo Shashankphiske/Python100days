67d22225d0a2a5edc8d72c78eeada625cf183f2f
Shashank Phiske <111897076+Shashankphiske@users.noreply.github.com>
2022-12-21 07:33:23 -0800

    Add files via upload

---
+++ Day8Forloops.py
@@ -0,0 +1,9 @@
+'''
+For loop : a for loop is used for iterating over a sequence (i.e a list,tuple,dictionary,set,or a string )
+           we can use range() function to make for loop for a limited number of times
+           we can use else statement with for loop once the loop is over and the else statement is executed
+           
+Nested loops : a nested loop is a loop inside a loop
+               the inner loop will be executed first and then the outer loop
+               
+              '''
